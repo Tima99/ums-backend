6fab08bec6341197170930273dcc017796918516
Amit <80810016+Tima99@users.noreply.github.com>
2023-10-05 20:55:48 +0530

    Update MyCourses.js

--- controllers/courses/MyCourses.js
+++ controllers/courses/MyCourses.js
@@ -3,14 +3,14 @@ const Course = require("./Course")
 async function MyCourses(req, res){
     try {
         const user = req.email
-        
+        console.log({user})
         const courses = await Course.findAll({user})
         
         res.json(courses)
         
     } catch (error) {
         console.log(error)
-        res.status(500).json("Something went wrong...")
+        res.status(500).json({message: "Something went wrong..."})
     }
 }
 
