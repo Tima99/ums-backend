e33157dc5331f00f53a583c67b0cde75fcfbf3ff
Amit <80810016+Tima99@users.noreply.github.com>
2023-10-05 19:48:56 +0530

    Update MyUsers.js

--- utils/MyUsers.js
+++ utils/MyUsers.js
@@ -3,13 +3,13 @@ const { query } = require("../db");
 async function MyUsers(admin) {
     try {
         const sql = `
-        SELECT  USERS.*, genders.gender, departments.name AS department,
+        SELECT  users.*, genders.gender, departments.name AS department,
         GROUP_CONCAT(
             (SELECT courses.name from courses WHERE courses.id = users_courses.id)
         ) as course
         FROM users 
         INNER JOIN departments ON users.departmentId  = departments.ID and users.admin='${admin}'
-        LEFT JOIN genders ON USERS.GENDERID = genders.ID
+        LEFT JOIN genders ON users.genderId = genders.ID
         LEFT JOIN users_courses on users_courses.user = users.email 
         GROUP By users.email;
         `;
@@ -19,7 +19,7 @@ async function MyUsers(admin) {
         return users
         
     } catch (error) {
-        Promise.reject(error);
+        return Promise.reject(error);
     }
 }
 
